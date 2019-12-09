d2ff5104d8c8737c8e55301c894c4219a3683d4b
iqbal rifai <iqbalrifai@waifu.club>
2019-12-09 12:55:58 +0700

    Update kecerahan.c
    Make can use 2 value or Percent digits

--- kecerahan.c
+++ kecerahan.c
@@ -1,60 +1,65 @@
 #include <stdio.h>
+/* silahkan edit 'DEVICES' dan sesuaikan dengan devices yang kalian punya */
+#define TARGET "/sys/class/backlight/DEVICES/brightness"
 
-int main(int argc, char *argv[]) {
+int cki(char * arr) {
+        int num = 0;
+        char sign;
+        short digit, mult = 1, offset = 0;
+        if ( * arr == '-') {
+                mult = -1;
+                offset++;
+        } else if ( * arr == '+') {
+                offset++;
+        }
+        while ( * (arr + offset) != '\0') {
+                digit = * (arr + offset) - 48;
+                num = num * 10 + digit;
+                offset++;
+        }
+        return num * mult;
+}
 
-	int val, inc;
-	char c;
+void ikc(int num, char * buf) {
+        short digit, offset = 0;
+        int save = num, div = 1;
+        while (save > 0) {
+                div *= 10;
+                save /= 10;
+        }
+        div /= 10;
 
-	if (argc != 2) {
-		printf("kecerahan: pakai: \"kecerahan {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, +, -}\"\n");
-		return 1;
-	} else if (argv[1][1] != '\0') {
-		printf("kecerahan: pakai: \"kecerahan {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, +, -}\"\n");
-		return 1;
-	} else {
-		switch (c=*argv[1]) {
-			case '0':
-			case '1':
-			case '2': /* for *argv[1] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9} */
-			case '3':
-			case '4':
-			case '5':
-			case '6':
-			case '7':
-			case '8':
-			case '9': val = c - '0'; inc = 0; break;
-			case '+': val = 1; inc = 1; break;
-			case '-': val = 1; inc =-1; break;
-			default: printf("kecerahan: pakai: \"kecerahan {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, +, -}\"\n"); break;
-		}
-	}
+        while (div >= 1) {
+                digit = num / div % 10;
+                div /= 10;
+                *(buf + offset) = digit + 48;
+                offset++;
+        }
+        *(buf + offset) = '\0';
+}
 
-	FILE *fp; /* fp = file pointer */
-	int br_val;
-
-	/* silahkan edit 'DEVICES' dan sesuaikan dengan devices yang kalian punya */
-	if ((fp = fopen("/sys/class/backlight/DEVICES/brightness", "r")) == NULL) {
-		printf("kecerahan: Tidak dapat membuka file.\n");
-		return 1;
-	} else {
-		fscanf(fp, "%d", &br_val);
-		fclose(fp);
-
-		int br_new = (inc==0) ? val : br_val + inc;
-		if (br_new > 7) {
-			printf("kecerahan: dalam maximal.\n");
-		} else if (br_new < 0) {
-			printf("kecerah: dalam minimal.\n");
-		}
-		
-		/* silahkan edit 'DEVICES' dan sesuaikan dengan devices yang kalian punya */
-		if ((fp = fopen("/sys/class/backlight/DEVICES/brightness", "w")) == NULL) {
-			printf("kecerahan: Tidak dapat membuka file sedang dalam menulis.\n");
-			return 1;
-		} else {
-			fprintf(fp, "%d", br_new);
-			fclose(fp);
-		}
-	}
-	return 0;
+int main(int argc, char ** argv) {
+        if (argc == 2) {
+                FILE * korban;
+                if (argv[1][0] != '-' && argv[1][0] != '+') {
+                        korban = fopen(TARGET, "w");
+                        fprintf(korban, "%s", argv[1]);
+                        fclose(korban);
+                } else {
+                        char buff[4];
+                        korban = fopen(TARGET, "r");
+                        fscanf(korban, "%s", buff);
+                        fclose(korban);
+                        int d = cki(buff);
+                        int step = cki(argv[1]);
+                        d += step;
+                        ikc(d, buff);
+                        korban = fopen(TARGET, "w");
+                        fprintf(korban, "%s", buff);
+                        fclose(korban);
+                }
+                return 0;
+        } else {
+                return -1;
+        }
 }
